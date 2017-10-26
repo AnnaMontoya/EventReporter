02276c9c0aade132ca4333b57afaa1103ef2fd83
AnnaMontoya <anna.l.montoya@gmail.com>
2017-10-26 08:45:52 -0600

    last push

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,60 +1,37 @@
 require 'csv'
 require "pry"
-require "./lib/attendee"
-require "./lib/queue"
+require_relative "attendee"
+require_relative "queue"
 
 class EventReporter
   attr_reader :queue
 
   def initialize
-  @queue     = Queue.new
-  @attendees = []
+    @queue     = Queue.new
+    @attendees = []
   end
 
   def load(filename = "full_event_attendees.csv")
     contents = CSV.open filename, headers: true, header_converters: :symbol
     contents.map do |row|
-      # binding.pry
     @attendees << Attendee.new(row)
-    # binding.pry
     end
   end
 
   def find(attribute, criteria)
-    you = @attendees.find_all do |attendee|
-      attendee.first_name == criteria
+    @attendees.find_all do |attendee|
+      if attendee.first_name.downcase == criteria.downcase
+        queue.data << attendee
+      end
     end
-    queue.data << you
-    binding.pry
+    queue.data
   end
-
-
-
-
 
 end
 
-#commands: load = Erase any loaded data and parse the specified file.
-              #  If no filename is given, default to full_event_attendees.csv.
-#          find (attribute, criteria) = "Populate the queue with all records matching the
-                            # criteria for the given attribute."
-#          queue = an array capable of outputting, clearing, printing, save, and export
-#          help = Output a listing of the available individual commands, and a description
-                  # of how to use the specific command
-
-# def clean_zipcode(zipcode)
-#   zipcode.to_s.rjust(5,"0")[0..4]
-# end
-#
-# puts "EventReporter initialized."
-#
-# contents = CSV.open './full_event_attendees.csv', headers: true, header_converters: :symbol
-#
-# contents.each do |row|
-#   # binding.pry
-#   name = row[:first_name]
-#
-#   zipcode = clean_zipcode(row[:zipcode])
-#
-#   puts "#{name} #{zipcode}"
-# end
+input = gets.chomp
+while (input!= 'exit')
+  EventReporter.new
+  if input == "find"
+  end
+end
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,5 @@
 require 'csv'
-require "./lib/attendee"
+# require_relative "./lib/attendee"
 require "pry"
 
 class Queue
@@ -13,17 +13,3 @@ class Queue
     count = data.count
   end
 end
-
-#   def load(filename = "full_event_attendees.csv")
-#   contents = CSV.open filename, headers: true, header_converters: :symbol
-#   contents.map do |row|
-#     # binding.pry
-#     @attendees << Attendee.new(row)
-#     binding.pry
-#     end
-#   end
-# end
-
-# Queue.new.load
-
-# line 12-15 load?
